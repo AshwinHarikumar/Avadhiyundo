5715603bb34a0a3ba5456b1a39c902dd625d82b7
Ashwin Harikumar <achuhari79@gmail.com>
2026-08-04 10:27:27 +0530

    style: improve text color contrast for grey text in dark mode

--- sw.js
+++ sw.js
@@ -6,13 +6,13 @@
  * fails we fall back to the cached copy, and app.js states its age out loud.
  */
 
-var CACHE = "krhw-v25";
+var CACHE = "krhw-v27";
 
 var SHELL = [
   "./",
   "./index.html",
-  "./app.css?v=25",
-  "./app.js?v=25",
+  "./app.css?v=27",
+  "./app.js?v=27",
   "./logo.png",
   "./manifest.webmanifest"
 ];
